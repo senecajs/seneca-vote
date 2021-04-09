71f2f230999f8ba50a3745f9c3c90935987c2cc0
lilsweetcaligula <3748714-lilsweetcaligula@users.noreply.gitlab.com>
2021-04-10 00:15:35 +0300

    Use a different symbol for the counter entity tag

--- plugins/throttle.js
+++ plugins/throttle.js
@@ -5,12 +5,12 @@ function throttle(opts = {}) {
   this.add('sys:throttle,cmd:throttle', async function (msg, reply) {
     await lock(async () => {
       const counter_entity = this.entity('sys/counter')
-      const existing_counter = await counter_entity.load$({ tag: '_' })
+      const existing_counter = await counter_entity.load$({ tag: '$' })
 
       const updated_counter = await (async () =>
         existing_counter || counter_entity
           .make$()
-          .data$({ tag: '_', value: -1 }))()
+          .data$({ tag: '$', value: -1 }))()
 
 
       Assert.object(updated_counter, 'updated_counter')
@@ -19,7 +19,7 @@ function throttle(opts = {}) {
       const cur_value = updated_counter.value
 
       const result = await updated_counter
-        .data$({ tag: '_', value: cur_value + 1 })
+        .data$({ tag: '$', value: cur_value + 1 })
         .save$()
 
       return reply(null, result)
