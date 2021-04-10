86a38a7633ec63c436639cbc6654c8511b136aa0
lilsweetcaligula <3748714-lilsweetcaligula@users.noreply.gitlab.com>
2021-04-10 19:53:42 +0300

    Improve validation in the GetPoll action

--- actions/get_poll.js
+++ actions/get_poll.js
@@ -1,48 +1,48 @@
 const Assert = require('assert-plus')
 const Poll = require('../entities/sys/poll')
-const { fetchProp } = require('../lib/utils')
 const Shapes = require('../lib/shapes')
+const { fetchProp } = require('../lib/utils')
+const { ValidationError } = require('../lib/errors')
 
 module.exports = function (opts = {}) {
   this.add('sys:vote,get:poll', async function (msg, reply) {
     try {
       // TODO:
       // - [ ] Use seneca-joi
-      // - [ ] Proper handling of the validation errors.
       //
-      await Shapes.validate(joi => joi.object({
+      const validateMessage = Shapes.makeValidator(joi => joi.object({
         poll_id: joi.string().max(64).required()
-      }).unknown(), msg, { stripUnknown: true })
-    } catch (err) {
-      if (err && err.isJoi) {
+      }).unknown(), { stripUnknown: true })
+
+      const safe_params = await validateMessage(msg)
+      const poll_id = fetchProp(safe_params, 'poll_id', Assert.string)
+
+      const poll_entity = Poll.entity({ seneca: this })
+      const poll = await poll_entity.load$({ id$: poll_id })
+
+      if (!poll) {
         return reply(null, {
           status: 'failed',
-          error: { message: err.message }
+          error: { message: 'Poll does not exist' }
         })
       }
 
-      return reply(err)
-    }
-
-    const poll_id = fetchProp(msg, 'poll_id', Assert.string)
-    const poll_entity = Poll.entity({ seneca: this })
- 
-    const poll = await poll_entity.load$({ id$: poll_id })
-
-    if (!poll) {
-      // TODO: Improve this code. Please use the following for details:
-      // https://github.com/voxgig/seneca-member/blob/429ca68fc07311d97a8f18a3fea3f43a24fa21db/member.js#L272
-      //
       return reply(null, {
-        status: 'failed',
-        error: { message: 'Poll does not exist' }
+        status: 'success',
+        data: { poll: poll.data$(false) }
       })
-    }
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        const error_message = fetchProp(err, 'message')
 
-    return reply(null, {
-      status: 'success',
-      data: { poll: poll.data$(false) }
-    })
+        return reply(null, {
+          status: 'failed',
+          error: { message: error_message }
+        })
+      }
+
+      throw err
+    }
   })
 }
 
