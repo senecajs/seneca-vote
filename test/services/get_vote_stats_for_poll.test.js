27067438a336ef23249cb7574062eb74909cd087
lilsweetcaligula <efim.bogdanovsky@gmail.com>
2021-05-17 22:38:39 +0300

    Improve test coverage of the vote stats calculation logic

--- test/services/get_vote_stats_for_poll.test.js
+++ test/services/get_vote_stats_for_poll.test.js
@@ -3,7 +3,7 @@ const Seneca = require('seneca')
 const Entities = require('seneca-entity')
 const SenecaPromisify = require('seneca-promisify')
 const Fixtures = require('../support/fixtures')
-const { fetchProp, yesterday } = require('../support/helpers')
+const { fetchProp, yesterday, now } = require('../support/helpers')
 const GetPollVoteStats = require('../../services/get_vote_stats_for_poll')
 
 fdescribe('the GetPollVoteStats service', () => { // fcs
@@ -70,7 +70,60 @@ fdescribe('the GetPollVoteStats service', () => { // fcs
               num_downvotes: 1
             }))
             return done()
-          }).catch(done)
+          })
+          .catch(done)
+      })
+    })
+
+    describe('a downvote exists for another poll', () => {
+      beforeEach(async () => {
+        await seneca.make('sys/vote')
+          .data$(Fixtures.vote({ type: 'down' }))
+          .save$()
+      })
+
+      it('returns correct vote counts for the requested poll', done => {
+        const si = senecaUnderTest(seneca, done)
+
+        GetPollVoteStats
+          .getVoteStatsForPoll({ poll_id }, { seneca: si })
+          .then(result => {
+            expect(result).toEqual(jasmine.objectContaining({
+              num_upvotes: 0,
+              num_downvotes: 0
+            }))
+            return done()
+          })
+          .catch(done)
+      })
+    })
+
+    describe('a voter upvoted on the requested poll, and downvoted on another', () => {
+      beforeEach(async () => {
+        await seneca.make('sys/vote')
+          .data$(Fixtures.vote({ type: 'up', poll_id }))
+          .save$()
+      })
+
+      beforeEach(async () => {
+        await seneca.make('sys/vote')
+          .data$(Fixtures.vote({ type: 'down' }))
+          .save$()
+      })
+
+      it('returns correct vote counts', done => {
+        const si = senecaUnderTest(seneca, done)
+
+        GetPollVoteStats
+          .getVoteStatsForPoll({ poll_id }, { seneca: si })
+          .then(result => {
+            expect(result).toEqual(jasmine.objectContaining({
+              num_upvotes: 1,
+              num_downvotes: 0
+            }))
+            return done()
+          })
+          .catch(done)
       })
     })
 
@@ -79,28 +132,352 @@ fdescribe('the GetPollVoteStats service', () => { // fcs
 
       beforeEach(async () => {
         await seneca.make('sys/vote')
-          .data$(Fixtures.vote({ type: 'up', poll_id, voter_id }))
+          .data$(Fixtures.vote({
+            type: 'up',
+            poll_id,
+            voter_id,
+            created_at: yesterday()
+          }))
           .save$()
       })
 
       beforeEach(async () => {
         await seneca.make('sys/vote')
-          .data$(Fixtures.vote({ type: 'down', poll_id, voter_id }))
+          .data$(Fixtures.vote({
+            type: 'down',
+            poll_id,
+            voter_id,
+            created_at: now()
+          }))
           .save$()
       })
 
-      fit('returns correct vote counts', done => { // fcs
-        const si = senecaUnderTest(seneca, done)
 
-        GetPollVoteStats
-          .getVoteStatsForPoll({ poll_id }, { seneca: si })
-          .then(result => {
-            expect(result).toEqual(jasmine.objectContaining({
-              num_upvotes: 0,
-              num_downvotes: 1
+      describe('no other voter exists', () => {
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 0,
+                num_downvotes: 1
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter upvoted once', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({ type: 'up', poll_id, voter_id: another_voter_id }))
+            .save$()
+        })
+
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 1,
+                num_downvotes: 1
+              }))
+              return done()
+            }).catch(done)
+        })
+      })
+
+      describe('another voter downvoted once', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({ type: 'down', poll_id, voter_id: another_voter_id }))
+            .save$()
+        })
+
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 0,
+                num_downvotes: 2
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter also - first upvoted, then changed his mind and downvoted', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'up',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: yesterday()
             }))
-            return done()
-          }).catch(done)
+            .save$()
+        })
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'down',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: now()
+            }))
+            .save$()
+        })
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 0,
+                num_downvotes: 2
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter first downvoted, then changed his mind and upvoted', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'down',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: yesterday()
+            }))
+            .save$()
+        })
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'up',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: now()
+            }))
+            .save$()
+        })
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 1,
+                num_downvotes: 1
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+    })
+
+    describe('a voter first downvoted, then changed his mind and upvoted', () => {
+      const voter_id = 'some_voter_id'
+
+      beforeEach(async () => {
+        await seneca.make('sys/vote')
+          .data$(Fixtures.vote({
+            type: 'down',
+            poll_id,
+            voter_id,
+            created_at: yesterday()
+          }))
+          .save$()
+      })
+
+      beforeEach(async () => {
+        await seneca.make('sys/vote')
+          .data$(Fixtures.vote({
+            type: 'up',
+            poll_id,
+            voter_id,
+            created_at: now()
+          }))
+          .save$()
+      })
+
+
+      describe('no other voter exists', () => {
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 1,
+                num_downvotes: 0
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter upvoted once', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({ type: 'up', poll_id, voter_id: another_voter_id }))
+            .save$()
+        })
+
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 2,
+                num_downvotes: 0
+              }))
+              return done()
+            }).catch(done)
+        })
+      })
+
+      describe('another voter downvoted once', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({ type: 'down', poll_id, voter_id: another_voter_id }))
+            .save$()
+        })
+
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 1,
+                num_downvotes: 1
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter first upvoted, then changed his mind and downvoted', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'up',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: yesterday()
+            }))
+            .save$()
+        })
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'down',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: now()
+            }))
+            .save$()
+        })
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 1,
+                num_downvotes: 1
+              }))
+              return done()
+            })
+            .catch(done)
+        })
+      })
+
+      describe('another voter also - first downvoted, then changed his mind and upvoted', () => {
+        const another_voter_id = 'some_id_of_another_voter'
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'down',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: yesterday()
+            }))
+            .save$()
+        })
+
+        beforeEach(async () => {
+          await seneca.make('sys/vote')
+            .data$(Fixtures.vote({
+              type: 'up',
+              poll_id,
+              voter_id: another_voter_id,
+              created_at: now()
+            }))
+            .save$()
+        })
+
+        it('returns correct vote counts', done => {
+          const si = senecaUnderTest(seneca, done)
+
+          GetPollVoteStats
+            .getVoteStatsForPoll({ poll_id }, { seneca: si })
+            .then(result => {
+              expect(result).toEqual(jasmine.objectContaining({
+                num_upvotes: 2,
+                num_downvotes: 0
+              }))
+              return done()
+            })
+            .catch(done)
+        })
       })
     })
 
@@ -116,7 +493,8 @@ fdescribe('the GetPollVoteStats service', () => { // fcs
               num_downvotes: 0
             }))
             return done()
-          }).catch(done)
+          })
+          .catch(done)
       })
     })
   })
